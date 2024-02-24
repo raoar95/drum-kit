0c2d96d284e31a0fe28e5d722dcadf1147cf6dad
Arun Rao <yarun1995.ar@gmail.com>
2024-02-24 23:39:16 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var drumBtn = document.querySelectorAll(".drum").length;
+
+for (i = 0; i < drumBtn; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    // Detecting Button Press
+    
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+    
+});
+
+}
+
+    // Detecting Key Press
+
+document.addEventListener("keypress", function(event){
+
+    makeSound(event.key);
+    buttonAnimation(event.key);    
+
+}); 
+
+    // Make Relevant sound with Key Press
+
+function makeSound(Key) {
+    
+    switch (Key) {
+    case "w":
+      var crash = new Audio ('sounds/crash.mp3')
+      crash.play();
+    break;
+
+    case "a":
+      var kick = new Audio ('sounds/kick-bass.mp3')
+      kick.play();
+    break;
+
+    case "s":
+      var snare = new Audio ('sounds/snare.mp3')
+      snare.play();
+    break;
+
+    case "d":
+      var tom1 = new Audio ('sounds/tom-1.mp3')
+      tom1.play();
+    break;
+
+    case "j":
+      var tom2 = new Audio ('sounds/tom-2.mp3')
+      tom2.play();
+    break;
+
+    case "k":
+      var tom3 = new Audio ('sounds/tom-3.mp3')
+      tom3.play();
+    break;
+
+    case "l":
+      var tom4 = new Audio ('sounds/tom-4.mp3')
+      tom4.play();
+    break;
+    
+    default:
+
+}
+
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout (function(){
+        activeButton.classList.remove("pressed");   
+    }, 100);
+
+}
